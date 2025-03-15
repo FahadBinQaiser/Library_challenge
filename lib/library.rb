59a1f11665875864752e9f4d9291b3d8a4cc3c30
Fahad <fahadbinqaiser3@gmail.com>
2025-03-15 16:20:06 +0500

    4 user stories done

--- lib/library.rb
+++ lib/library.rb
@@ -1,14 +1,33 @@
 require 'json'
+require 'date'
+
 class Library
+  attr_reader :books
   def initialize
-    file = File.read('./lib/books.json')
-    @books = JSON.parse(file, symbolize_names: true)
+    load_books
   end
   def add_books(book) 
     @books << book
+    save_books
   end
   def list_books
     available_books = @books.select { |book| book[:available] }
     available_books.map { |book| "#{book[:title]} by #{book[:author]}" }
   end  
+  def load_books
+    if File.exist?('./lib/books.json')
+      file = File.read('./lib/books.json')
+      @books = JSON.parse(file, symbolize_names: true)
+    else
+      books = []
+    end
+  end
+  def save_books
+    File.write('./lib/books.json', JSON.pretty_generate(@books))
+  end
+  def checkout_book(title)
+    book  = @books.find { |book| book[:title] == title }
+    book[:available] = false if book && book[:available]
+    book[:return_date] = Date.today + 30
+  end
 end
--- spec/spec_library.rb
+++ spec/spec_library.rb
@@ -4,4 +4,21 @@ describe Library do
   it 'shows a list of available books' do 
     expect(library.list_books).to eq(["The Hobbit by J.R.R. Tolkien"])  
   end
+  it 'loads book from the json file' do 
+    expect(library.books).not_to be_empty
+  end
+  it 'allows users to check out a book if it is available' do  
+    book_title = 'The Hobbit'
+    library.checkout_book(book_title)
+
+    available_books = library.list_books
+    expect(available_books).not_to include("The Hobbit by J.R.R Tolkien")
+  end
+  it 'returns a date when a book is checked out' do
+    library.checkout_book('The Hobbit')
+    book = library.books.find { |book| book[:title] == 'The Hobbit' }
+    expect(book[:return_date]).to eq(Date.today + 30)
+    expect(book[:available]).to eq false
+  end
+  
 end
