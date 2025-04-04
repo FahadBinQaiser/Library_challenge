71498d0ac39f830246de05229e36ced2de897ea9
Fahad <fahadbinqaiser3@gmail.com>
2025-04-04 16:59:01 +0500

    Fixed IT block structuring

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,8 +7,10 @@ describe Person do
   let(:person) { Person.new('Fahad') }
   let(:library) { Library.new }
 
-  it 'creates a person with a name and an empty list of books' do
+  it 'creates a person with a name' do
     expect(person.name).to eq('Fahad')
+  end
+  it 'initializes with empty books' do
     expect(person.books_list).to be_empty
   end
 end
